5e3f483f69495e6d18c645e8247db174b5c893bb
yadavgulshan <yadavgulshan542@gmail.com>
2024-06-09 05:32:40 +0530

    Added bandit program, based on greedy epsilon

---
+++ mutli-armed-bandit/bandit.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+NUM_TRIALS = 1000
+EPS = 0.1
+BANDITS_PROB = [0.2, 0.5, 0.75]
+
+
+class Bandit:
+    def __init__(self, p) -> None:
+        self.p = p
+        self.p_estimate = 0
+        self.N = 0
+
+    def pull(self):
+        return np.random.random() < self.p
+
+    def update(self, x):
+        self.N += 1
+        self.p_estimate = self.p_estimate + (1 / self.N) * (x - self.p_estimate)
+
+
+def experiment():
+    bandits = [Bandit(p) for p in BANDITS_PROB]
+    rewards = np.zeros(NUM_TRIALS)
+    num_times_explored = 0
+    num_times_exploited = 0
+    num_optimal = 0
+    optimal_j = np.argmax([b.p for b in bandits])
+    print("optimal j:", optimal_j)
+    for i in range(NUM_TRIALS):
+
+        if np.random.random() < EPS:
+            num_times_explored += 1
+            j = np.random.choice(len(bandits))
+
+        else:
+            num_times_exploited += 1
+            j = optimal_j
+
+        x = bandits[j].pull()
+        rewards[i] = x
+        bandits[j].update(x)
+
+    # print mean estimates for each bandit
+    for b in bandits:
+        print("mean estimate:", b.p_estimate)
+
+    # print total reward
+    print("total reward earned:", rewards.sum())
+    print("overall win rate:", rewards.sum() / NUM_TRIALS)
+    print("num_times_explored:", num_times_explored)
+    print("num_times_exploited:", num_times_exploited)
+    print("num times selected optimal bandit:", num_optimal)
+
+    cumulative_reward = np.cumsum(rewards)
+    win_rate = cumulative_reward / (np.arange(NUM_TRIALS) + 1)
+    plt.plot(win_rate)
+    plt.plot(np.ones(NUM_TRIALS) * np.max(BANDITS_PROB))
+    plt.show()
+
+
+if __name__ == "__main__":
+    experiment()
